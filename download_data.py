344349fec3023883ea4bf9bf210b08dd31f6fcd3
Yoav Ram <yoav@yoavram.com>
2018-10-26 13:03:44 +0300

    bypass ssl issues on aws

--- download_data.py
+++ download_data.py
@@ -1,16 +1,22 @@
 import keras
 from tensorflow.examples.tutorials.mnist import input_data
-from urllib.request import urlretrieve
+import requests
 import os
+import shutil
 import zipfile
+
+def download_file(url, fname):
+    r = requests.get(url, stream=True, verify=False)
+    with open(fname, 'wb') as f:
+        shutil.copyfileobj(r.raw, f)
 
 # MNIST and Fasihon MNIST, day 2 and 3
 print('* MNIST (Keras)...')
 keras.datasets.mnist.load_data()
 print('* Fashion-MNIST (Keras)...')
 keras.datasets.fashion_mnist.load_data()
-print('* MNIST (TensorFlow)...')
-input_data.read_data_sets('MNIST_data', one_hot=True)
+#print('* MNIST (TensorFlow)...')
+#input_data.read_data_sets('MNIST_data', one_hot=True)
 
 # ResNet50, day 3
 print('ResNet50')
@@ -21,7 +27,7 @@ url = 'https://github.com/karoldvl/ESC-50/archive/master.zip'
 fname = 'data/ESC.zip'
 if not os.path.exists(fname):
 	print('* ESC-50...')
-	urlretrieve(url, fname)
+	download_file(url, fname)
 	with zipfile.ZipFile(fname) as z:
 		z.extractall('data/')
 
@@ -30,8 +36,6 @@ url = 'https://github.com/yoavram/SpeechEmotion/archive/master.zip'
 fname = 'data/SpeechEmotion.zip'
 if not os.path.exists(fname):
 	print('* SpeechEmotion...')
-	urlretrieve(url, fname)
+	download_file(url, fname)
 	with zipfile.ZipFile(fname) as z:
 		z.extractall('data/')
-	
-# conda install librosa # for day 3
