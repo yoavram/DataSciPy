524b11f546a973e28655a40e07ee566161cacaf2
Yoav Ram <yoav@yoavram.com>
2019-07-14 18:22:10 +0300

    part of the previous update to logistic model session

--- solutions/logistic-model.py
+++ solutions/logistic-model.py
@@ -1,22 +1,21 @@
-def _cross_entropy(df, coefs):
+def _cross_entropy(X, Y, a):
+    nsamples = Y.shape[0] 
+    Z = logodds(X, a)
     return -sum(
         -z*(1-y) - np.log(1+np.exp(-z)) 
         for z, y 
-        in zip(logodds(df, coefs), df['Survived'].values)
-    ) / df.shape[0]
+        in zip(Z, Y)
+    ) / nsamples
         
-def cross_entropy(df, coefs):
-    y = df["Survived"].values
-    z = logodds(df, coefs)
-    logliks = -z * (1 - y) - np.log(1 + np.exp(-z))
+def cross_entropy(X, Y, a):
+    Z = logodds(X, a)
+    logliks = -Z * (1 - Y) - np.log(1 + np.exp(-Z))
     return -logliks.mean()
 
-def gradient_descent(df, coefs, η=0.01):
-    X = df[['Sex', 'Age', 'Pclass']].values
-    Y = df['Survived'].values
+def gradient_descent(X, Y, a, η=0.01):
     nsamples = Y.shape[0]
     
-    Yhat = expit(logodds(df, coefs))
+    Yhat = expit(logodds(X, a))
     δ = Yhat - Y
     grad = X.T @ δ / nsamples
     assert grad.shape == grad.shape
