e5028056b6eeaedfb36985c418dcbfaee5d20c0f
Yoav Ram <yoav@yoavram.com>
2019-07-18 14:58:37 +0300

    removed predict

--- solutions/softmax-model.py
+++ solutions/softmax-model.py
@@ -1,21 +1,4 @@
-import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression
-
-
-def softmax(x):
-    expx = np.exp(x - x.max(axis=1, keepdims=True))
-    return expx / expx.sum(axis=1, keepdims=True)
-
-
-def predict(W, X):
-    if X.ndim == 1: 
-        # if we got a single sample, so X is 1D we convert X to 2D with just one row
-        # to be consistent with cases in which we get multiple samples (rows)
-        X = X[np.newaxis, :]
-    Z = X @ W
-    Yhat = softmax(Z)
-    return Yhat
 
 def sklearn_softmax_model(X, Y):
     model = LogisticRegression(C=1e12, multi_class='multinomial', solver='lbfgs', n_jobs=-1)
