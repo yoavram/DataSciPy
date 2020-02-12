374f2c814e9ebfafdc7b68bb9c74f07eed02687b
Yoav Ram <yoav@yoavram.com>
2020-02-12 09:09:19 +0200

    remove url stuff

--- solutions/logistic-tennis.py
+++ solutions/logistic-tennis.py
@@ -1,6 +1,4 @@
 import zipfile
-import urllib.request
-import os.path
 import pickle
 
 import numpy as np
@@ -8,16 +6,13 @@ import pandas as pd
 from sklearn import model_selection
 from sklearn.linear_model import LogisticRegression
 
-data_url = 'https://github.com/ipython-books/cookbook-data/raw/master/tennis.zip'
-data_filename = '../data/tennis.zip'
+filename = '../data/tennis.zip'
 player = 'Rafael-Nadal'
 features = ['player1 aces', 'player1 double faults']
 model_filename = 'logistic_tennis.model'
 
 if __name__ == '__main__':
-	if not os.path.exists(data_filename):
-	    urllib.request.urlretrieve(data_url, data_filename)
-	tennis_zip = zipfile.ZipFile(data_filename)    
+	tennis_zip = zipfile.ZipFile(filename)    
 	path = os.path.join('data', '{}.csv')
 	path = path.format(player.replace(' ', '-'))
 	with tennis_zip.open(path) as f:
@@ -37,4 +32,5 @@ if __name__ == '__main__':
 	with open(model_filename, 'wb') as f:
 		pickle.dump(model, f)
 		print("Saving model to", model_filename)
+	print('Prediction:')
 	print(X_test.iloc[0], y_test.iloc[0])
