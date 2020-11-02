4a5d02f40f750f116bb9d61d959fbdc8720fa366
Yoav Ram <yoav@yoavram.com>
2020-11-02 16:51:21 +0200

    revide for lam; iris->seeds; load digits from sklearn

--- solutions/logistic-model.py
+++ solutions/logistic-model.py
@@ -1,16 +1,10 @@
-def _cross_entropy(X, Y, a):
-    nsamples = Y.shape[0] 
-    Z = logodds(X, a)
-    return -sum(
-        -z*(1-y) - np.log(1+np.exp(-z)) 
-        for z, y 
-        in zip(Z, Y)
-    ) / nsamples
-        
 def cross_entropy(X, Y, a):
     Z = logodds(X, a)
     logliks = -Z * (1 - Y) - np.log(1 + np.exp(-Z))
     return -logliks.mean()
+
+
+
 
 def gradient_descent(X, Y, a, η=0.01):
     nsamples = Y.shape[0]
@@ -18,6 +12,6 @@ def gradient_descent(X, Y, a, η=0.01):
     Z = X @ a
     Yhat = 1 / (1 + np.exp(-Z))
     δ = Yhat - Y
-    grad = X.T @ δ / nsamples
-    assert grad.shape == a.shape
-    return a - η * grad
+    dJda = X.T @ δ / nsamples
+    assert dJda.shape == a.shape
+    return a - η * dJda
