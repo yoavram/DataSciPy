a565e30d34b08fa3d00a35a30cfa028f52411686
Yoav Ram <yoav@yoavram.com>
2025-07-27 19:03:01 +0300

    updated

--- scripts/tk_matplotlib.py
+++ scripts/tk_matplotlib.py
@@ -1,30 +1,30 @@
-import tkinter # in Python 2 use import Tkinter as tkinter
+import tkinter as tk	
 import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
 
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg as FigureCanvas
-from matplotlib.backends.backend_tkagg import NavigationToolbar2TkAgg as NavigationToolbar
+from matplotlib.backends.backend_tkagg import NavigationToolbar2Tk as NavigationToolbar
 
 import numpy as np
 
-class MainWindow:
-	def __init__(self, master=None):
-		self.frame = tkinter.Frame(master)
+class Application:
+	def __init__(self):
+		self.root = tk.Tk()
+		self.frame = tk.Frame(self.root)
 		self.frame.pack(padx=15,pady=15)
 
-		self.fig = matplotlib.figure.Figure(figsize=(6, 4), dpi=100)
+		self.fig = Figure(figsize=(6, 4), dpi=100)
 		self.ax = self.fig.add_subplot(111)
-		self.ax.plot(np.linspace(0, 4), np.sin(np.linspace(0, 4)))
+		x = np.linspace(0, 4)
+		self.ax.plot(x, np.sin(x))
 		self.canvas = FigureCanvas(self.fig, master=self.frame)
-		self.canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
-
-		self.mpl_toolbar = NavigationToolbar(self.canvas, master)
+		self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+		self.mpl_toolbar = NavigationToolbar(self.canvas, self.frame)
 		self.mpl_toolbar.update()
-		self.canvas._tkcanvas.pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
-
+		self.canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
 
 if __name__ == '__main__':
-	app = tkinter.Tk()
-	window = MainWindow(app)
-	app.mainloop()
+    app = Application()
+    app.root.mainloop()
